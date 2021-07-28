29cd3b5e628937cc48fe5401110e13f3005652a4
Alfredo Gabriel <amgabriel1@hotmail.com>
2021-07-28 12:08:58 -0300

    sadas

--- C++/quest_three.cpp
+++ C++/quest_three.cpp
@@ -5,19 +5,18 @@
 using namespace std;
 
 int main() {
-    setlocale(LC_ALL, "Portuguese");
-    int i;
+    /*setlocale(LC_ALL, "Portuguese");
     unsigned char texto;
     texto = 138;
     printf("Digite uma letra: ");
     cout << "A letra é no cout " << texto << "\n";
     printf("A letra é %c\n", char.size);
-    /*
+    */
+    int i;
     for(i=32;i<255;i++){
         if(i%10==0)
             printf("\n");
             printf("%d = %c\t", i, i);
     }
     printf("\n");
-    */
 }
