1e314c4639df582ef896c9550325b153de479411
alverad katsuro <amgabriel1@hotmail.com>
2021-08-14 23:34:20 -0300

    if

--- Java_start/atividade_five/classes/Fatorial.java
+++ Java_start/atividade_five/classes/Fatorial.java
@@ -2,10 +2,14 @@ package classes;
 
 public class Fatorial {
     public static int fatorial(int x1) {
-        int result = 1;
-        for (int i = 1; i <= x1; i++) {
-            result *= i;
+        if (x1 >= 0) {
+            int result = 1;
+            for (int i = 1; i <= x1; i++) {
+                result *= i;
+            }
+            return result;    
+        } else {
+            return -1;
         }
-        return result;
     }
 }
