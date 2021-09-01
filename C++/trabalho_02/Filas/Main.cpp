9dd020ddf26085991980a8cba9403b507c3c088e
alverad katsuro <amgabriel1@hotmail.com>
2021-09-01 17:03:07 -0300

    go teste mais bonito

--- C++/trabalho_02/Filas/Main.cpp
+++ C++/trabalho_02/Filas/Main.cpp
@@ -3,13 +3,13 @@
 
 int main(int argc, char const *argv[]){
     Fila fila = inicializa();
+    Enqueue(1, &fila);
+    Enqueue(2, &fila);
+    Enqueue(3, &fila);
+    Enqueue(4, &fila);
     Enqueue(5, &fila);
-    Enqueue(5, &fila);
-    Enqueue(5, &fila);
-    Enqueue(56, &fila);
-    Enqueue(5, &fila);
-    Enqueue(5, &fila);
-    Enqueue(5, &fila);
+    Enqueue(6, &fila);
+    Enqueue(7, &fila);
     furarFila(100, &fila);
     showQueue(&fila);
     return 0;
